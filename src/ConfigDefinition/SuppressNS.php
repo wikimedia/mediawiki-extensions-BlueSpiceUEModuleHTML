5e9c7329205a636591f7bac33ff0fdd558a7a25a
Dmitry Shevliakov <dsh@astarios.com>
2019-07-22 14:32:21 +0300

    Initial commit
    Change-Id: Ic13902b3ddb2a3683a61f0d78a2b0ca77351eb4f

---
+++ src/ConfigDefinition/SuppressNS.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BlueSpice\UEModuleHTML\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition\BooleanSetting;
+
+class SuppressNS extends BooleanSetting {
+
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_EXPORT . '/BlueSpiceUEModuleHTML',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceUEModuleHTML/' . static::FEATURE_EXPORT,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_PRO . '/BlueSpiceUEModuleHTML',
+		];
+	}
+
+	public function getLabelMessageKey() {
+		return 'bs-uemodulehtml-pref-SuppressNS';
+	}
+}
---
+++ src/Hook/BSMigrateSettingsFromDeviatingNames/SkipServiceSettings.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BlueSpice\UEModuleHTML\Hook\BSMigrateSettingsFromDeviatingNames;
+
+class SkipServiceSettings extends \BlueSpice\Hook\BSMigrateSettingsFromDeviatingNames {
+
+	protected function skipProcessing() {
+		if( in_array( $this->oldName, $this->getSkipSettings() ) ) {
+			return false;
+		}
+		return true;
+	}
+
+	protected function doProcess() {
+		$this->skip = true;
+	}
+
+	protected function getSkipSettings() {
+		return [
+			'MW::UEModuleHTML::TemplatePath'
+		];
+	}
+}
