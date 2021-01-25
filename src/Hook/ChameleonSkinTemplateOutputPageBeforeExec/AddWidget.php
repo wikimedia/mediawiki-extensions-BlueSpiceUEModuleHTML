639a8a99afa07daeb2408099cb1c0a889b056ecc
pwirth <wirth@hallowelt.biz>
2021-01-25 14:23:39 +0100

    Replace deprecated hook handler base ChameleonSkinTemplateOutputPageBeforeExec
    [4.0]
    
    ERM 22695
    
    Change-Id: I05b76f56e0c09f58f423b3d3bfcfe04db4cba34d

--- src/Hook/ChameleonSkinTemplateOutputPageBeforeExec/AddWidget.php
+++ src/Hook/ChameleonSkinTemplateOutputPageBeforeExec/AddWidget.php
@@ -2,7 +2,7 @@
 
 namespace BlueSpice\UEModuleHTML\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
 
-use BlueSpice\Calumma\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
+use BlueSpice\Hook\ChameleonSkinTemplateOutputPageBeforeExec;
 
 class AddWidget extends ChameleonSkinTemplateOutputPageBeforeExec {
 	/**
